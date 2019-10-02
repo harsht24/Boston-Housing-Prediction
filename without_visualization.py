5e0173ca6ff31351ae5fb49be480f221355c4175
Harsh Tamkiya <32032491+harsht24@users.noreply.github.com>
2019-10-02 21:20:21 +0530

    Update without_visualization.py

--- without_visualization.py
+++ without_visualization.py
@@ -6,37 +6,36 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
-r2 = []
+
 dataset = load_boston()
 
 features = pd.DataFrame(dataset.data, columns=dataset.feature_names)
 features['MEDV'] = dataset.target
 
-for l in range(30):
-    min_max_scaler = preprocessing.MinMaxScaler()
-    column_sels = ['LSTAT', 'PTRATIO', 'RM', 'DIS']
 
-    new_features = features.loc[:, column_sels]
+min_max_scaler = preprocessing.MinMaxScaler()
+column_sels = ['LSTAT', 'PTRATIO', 'RM', 'DIS']
 
-    x = pd.DataFrame(data=min_max_scaler.fit_transform(new_features), columns=column_sels)
-    y = features['MEDV']
-   
-    # removing skewness from data through log transform..
+new_features = features.loc[:, column_sels]
 
-    y = np.log1p(y)
-    for i in x.columns:
-        if np.abs(x[i].skew()) > 0.3:
-            x[i] = np.log1p(x[i])
-    
+x = pd.DataFrame(data=min_max_scaler.fit_transform(new_features), columns=column_sels)
+y = features['MEDV']
 
-    train_X, test_X, train_y, test_y = train_test_split(x, y, test_size=0.3)
-    model = LinearRegression()
-    model.fit(train_X, train_y)
-    prediction = model.predict(test_X)
+# removing skewness from data through log transform..
 
-    score = r2_score(prediction, test_y)
-    r2.append(score)
+y = np.log1p(y)
+for i in x.columns:
+    if np.abs(x[i].skew()) > 0.3:
+        x[i] = np.log1p(x[i])
 
-print(r2)
-print("max = ",max(r2))
-print("min = ",min(r2))
+# spliting data into training set and test set.
+
+train_X, test_X, train_y, test_y = train_test_split(x, y, test_size=0.3)
+model = LinearRegression()
+model.fit(train_X, train_y)
+prediction = model.predict(test_X)
+
+score = r2_score(prediction, test_y)
+
+
+print(score)
